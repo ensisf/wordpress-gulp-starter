c98967353e387f183fccd1ce8280f0fa841b4200
Bohdan <zamchenco@gmail.com>
2017-08-21 13:39:30 +0300

    make module map

--- src/js/devs/map.js
+++ src/js/devs/map.js
@@ -1,39 +1,41 @@
-; //element id = map, data-zoom, data-marker, data-lats, data-lons, data-title, data-address
-var mapElement = document.getElementById('map');
-if (window.mapElement) {
-    var map,
-        dSet = mapElement.dataset,
+ //element id = map, data-zoom, data-marker, data-lats, data-lons, data-title, data-address
+ export default function () {
+     const mapElement = document.getElementById('map');
+     
+     if (mapElement) {
+         const dSet = mapElement.dataset;
 
-        lats = JSON.parse(dSet.lats),
-        lons = JSON.parse(dSet.lons),
+         const lats = JSON.parse(dSet.lats);
+         const lons = JSON.parse(dSet.lons);
 
-        titles = JSON.parse(dSet.title),
-        address = JSON.parse(dSet.address); 
+         const titles = JSON.parse(dSet.title);
+         const address = JSON.parse(dSet.address);
 
-    function initialize() {
-        map = new google.maps.Map(mapElement, {
-            center: new google.maps.LatLng(lats[0], lons[0]),
-            zoom: parseFloat(dSet.zoom)
-        });
+         function initialize() {
+            const map = new google.maps.Map(mapElement, {
+                 center: new google.maps.LatLng(lats[0], lons[0]),
+                 zoom: parseFloat(dSet.zoom)
+             });
 
-        lats.forEach(function (el, i, lats) {
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(lats[i], lons[i]),
-                icon: dSet.marker,
-                map: map
-            });
+             lats.forEach((el, i, lats) => {
+                 const marker = new google.maps.Marker({
+                     position: new google.maps.LatLng(lats[i], lons[i]),
+                     icon: dSet.marker,
+                     map: map
+                 });
 
-            var infowindow = new google.maps.InfoWindow({
-                content: '<div class="bubble"><strong>' + titles[i] + '</strong><p>' + address[i] + '</p></div>'
-            });
+                 const infowindow = new google.maps.InfoWindow({
+                     content: '<div class="bubble"><strong>' + titles[i] + '</strong><p>' + address[i] + '</p></div>'
+                 });
 
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
+                 marker.addListener('click', function () {
+                     infowindow.open(map, marker);
+                 });
 
-            infowindow.open(map, marker);
-        })
-    }
+                 infowindow.open(map, marker);
+             })
+         }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-}
+         google.maps.event.addDomListener(window, 'load', initialize);
+     }
+ }
